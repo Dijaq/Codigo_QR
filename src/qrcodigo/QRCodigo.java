8c7393bc10651ef24b6b07837840338d08dc6ae8
Diego <dijaq08@gmail.com>
2016-10-02 09:50:06 -0500

    "nueva rama sabiendo como trabajar jajaja"

--- src/qrcodigo/QRCodigo.java
+++ src/qrcodigo/QRCodigo.java
@@ -16,7 +16,7 @@ public class QRCodigo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Estamos probando que no haya conflictos con la rama principal");
+        System.out.println("Estamos probando si funciona");
     }
     
 }
